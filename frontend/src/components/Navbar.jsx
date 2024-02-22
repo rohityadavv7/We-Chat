fd0e90326d514f1a56d0ecfe6f6a84add67d66f8
Rohit Yadav <7rohit.y@gmail.com>
2024-02-22 20:31:49 +0530

    Update Navbar.jsx (heading)

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className='w-11/12 mx-auto flex justify-between'>
         <h1 className='bg flex gap-4 ml-16 items-center justify-start font-semibold text-3xl mx-auto'>
-            <p className='bg text-white mt-4 font-inter'>We-Chat</p>
+            <p className='bg text-white mt-4 font-inter'>V-Chat</p>
             <IoIosArrowForward className='text-white mt-4'/>
         </h1>
 
